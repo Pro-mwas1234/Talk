f6fc6581daf442108f8082c4a3dff0bd455f8bbe
Pro-mwas1234 <130271674+Pro-mwas1234@users.noreply.github.com>
2025-06-26 12:51:35 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Serve static files
-app.use(express.static(path.join(__dirname, '/')));
+// Serve static files from public directory
+app.use(express.static(path.join(__dirname, 'public')));
 
-// All routes point to chat.html
+// All other routes serve chat.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'chat.html'));
+  res.sendFile(path.join(__dirname, 'public', 'chat.html'));
 });
 
 app.listen(port, () => {
-  console.log(`Chat app running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
